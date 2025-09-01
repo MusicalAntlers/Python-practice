df6730032491139af2fc201d563ab9ded034cac1
Morgan Williams <morgwilliams7@gmail.com>
2025-09-01 15:25:45 -0500

    rps with recursion

--- lesson10/rps3.py
+++ lesson10/rps3.py
@@ -3,32 +3,26 @@ import random
 from enum import Enum
 
 
-class RPS(Enum):
-    ROCK = 1
-    PAPER = 2
-    SCISSORS = 3
-
-
-playagain = True
-
-while playagain:
-    
+def play_rps():
+    class RPS(Enum):
+        ROCK = 1
+        PAPER = 2
+        SCISSORS = 3
 
     playerchoice = input("Enter...\n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
 
-    player = int(playerchoice)
+    if playerchoice not in ["1", "2", "3"]:
+        print("You must enter 1, 2, or 3.")
+        return play_rps
 
-    if player < 1 or player > 3:
-        sys.exit("You must enter 1, 2, or 3.")
+    player = int(playerchoice)
 
     computerchoice = random.choice("123")
 
     computer = int(computerchoice)
 
-    print("")
-    print("You chose " + str(RPS(player)).replace("RPS.", "") + ".")
-    print("Python chose " + str(RPS(computer)).replace("RPS.", "") + ".")
-    print("")
+    print("\nYou chose " + str(RPS(player)).replace("RPS.", "").title() + ".")
+    print("Python chose " + str(RPS(computer)).replace("RPS.", "").title() + ".\n")
 
     if player == 1 and computer == 3:
         print("🎉You win!")
@@ -40,3 +34,22 @@ while playagain:
         print("😮Tie game!")
     else:
         print("🐍Python wins!")
+
+    print("\nPlay again?")
+
+    while True:
+        playagain = input("\nY for Yes or \nQ to quit\n")
+        if playagain.lower() not in ["y", "q"]:
+            continue
+        else:
+            break
+
+    if playagain.lower() == "y":
+        return play_rps()
+    else:
+        print("\n🎉🎉🎉🎉")
+        print("Thank you for playing!\n")
+        sys.exit("Bye! 👋")
+
+
+play_rps()
